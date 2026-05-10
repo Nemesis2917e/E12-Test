d6f8d51bb9cea39edf6bc45ae41519936d7ec4f2
Saad Arshad <robotichuman830@gmail.com>
2026-05-10 13:17:30 +0500

    Add files via upload

---
+++ users.js
@@ -0,0 +1,22 @@
+
+
+const users = [
+  {
+    name: "Admin",
+    username: "admin",
+    password: "admin",
+    earnings: 100
+  },
+  {
+    name: "Saad",
+    username: "saad99",
+    password: "1111",
+    earnings: 600
+  },
+  {
+    name: "Ahmed",
+    username: "ahmed7",
+    password: "9999",
+    earnings: 1200
+  }
+];
